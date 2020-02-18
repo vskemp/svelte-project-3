962aab908032855dd8667e9c5dc782a825d30396
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2020-02-18 15:40:52 -0500

    meet us header

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,30 @@
 <script>
+    import Header from './UI/Header.svelte';
 
+    const meetups = [
+        {
+            id: 'm1',
+            title: 'Coding Bootcamp',
+            subtitles: 'Learn to code in 2 hours',
+            description: 'In this meetup, we will have some experts teach you how to code',
+            imageURL: 'https://static.tvtropes.org/pmwiki/pub/images/KRtD_Kirby_portriat_1669.png',
+            address: '20 Nerd Road, 492382 Atlanta',
+            contactEmail: 'code@text.com'
+        }
+        {
+            id: 'm2',
+            title: 'Swimmin',
+            subtitles: 'Let\'s Go Swimmin',
+            description: 'In this meetup, we will swim',
+            imageURL: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a853868b-c1ae-4db1-8daf-405f4b0e19e7/d7ytwhq-b4fe868e-50af-4f33-8daa-d8a9c845be72.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2E4NTM4NjhiLWMxYWUtNGRiMS04ZGFmLTQwNWY0YjBlMTllN1wvZDd5dHdocS1iNGZlODY4ZS01MGFmLTRmMzMtOGRhYS1kOGE5Yzg0NWJlNzIucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.lseWhv118DS00Ae-d9NaOiMh7Q5ZTJrZWhgQRk-NT4Q',
+            address: '420 Battle St, 823822 New York',
+            contactEmail: 'swim@text.com'
+        }
+    ];
 </script>
 
-<style>
 
-</style>
+<Header />
 
-<h1>Time for a project!</h1>
+
+
